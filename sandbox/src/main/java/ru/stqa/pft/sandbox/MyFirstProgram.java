3f9917a77fc6e1c66fbfb17f8785db91de786842
exarcist12 <exarcist12@gmail.com>
2018-07-10 20:57:05 +0400

    Первое решение Второго задания

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,33 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("World");
+    hello("Stas");
+    System.out.println();
+
+    Square s = new Square(5);
+
+    System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,5);
+
+    System.out.println("Площадь прямоугольника равна " + r.area ());
+
+
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(4,5);
+    System.out.println("расстояние между точками p1 и p2 равно "+ distance(p1,p2) );
+
   }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+  }
+
 
 }
