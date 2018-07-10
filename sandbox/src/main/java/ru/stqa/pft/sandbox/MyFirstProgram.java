a111ef0ff1d59b45497c74a05e59eca37462e489
exarcist12 <exarcist12@gmail.com>
2018-07-10 21:00:46 +0400

    Второе решение Второго задания

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -18,7 +18,7 @@ public class MyFirstProgram {
 
     Point p1 = new Point(1,1);
     Point p2 = new Point(4,5);
-    System.out.println("расстояние между точками p1 и p2 равно "+ distance(p1,p2) );
+    System.out.println("расстояние между точками p1 и p2 равно "+ p1.distance(p2) );
 
   }
 
@@ -27,9 +27,7 @@ public class MyFirstProgram {
     System.out.println("Hello," + somebody + "!");
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
-  }
+
 
 
 }
