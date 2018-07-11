7f4d5fb79d5c687b999fe86aa0a8646dc78fe2ca
exarcist12 <exarcist12@gmail.com>
2018-07-11 20:25:40 +0400

    Добавлено несколько тестов к третьему заданию

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,5 +9,11 @@ public class PointTests {
     Point p1 = new Point (1,1);
     Point p2 = new Point (4,5);
     Assert.assertEquals(p1.distance(p2),5.0);
+    Point p3 = new Point (-1,-1);
+    Point p4 = new Point (-4,-5);
+    Assert.assertEquals(p3.distance(p4),5.0);
+    Point p5 = new Point (-1,1);
+    Point p6 = new Point (-4,5);
+    Assert.assertEquals(p5.distance(p6),5.0);
   }
 }
