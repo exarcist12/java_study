013710cbe96d7d173a8f58ff95e8da52802990ca
exarcist12 <exarcist12@gmail.com>
2018-08-06 19:51:56 +0400

    Revert "4.8 error"
    This reverts commit 127b8d5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -100,8 +100,8 @@ public class ContactHelper extends HelperBase{
     List<ContactData> contacts = new ArrayList<ContactData>();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
-      String  name = element.getText();
-      int id = Integer.parseInt(element.findElement(By.tagName("td")).getAttribute("value"));
+      String  name = "Stas";
+      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       ContactData contact = new ContactData (id, name, "Petrov", "Vodkin", "Stas", "Stas", "BSS", "LibertyCity", "70001112233", "81112223334", "SS", "45678912300", "exarcist12@yandex.ru", "yandex.ru", "test", "test", "test", "FirstTestGroup");
       contacts.add (contact);
     }
