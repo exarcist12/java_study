924622b5a94aef6992712e2f4fd80eeb3381f676
exarcist12 <exarcist12@gmail.com>
2018-08-21 20:19:06 +0400

    6.9 отдельный конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -54,18 +54,20 @@ public class ContactDataGenerator {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try ( Writer writer = new FileWriter(file);){
+      writer.write(xml);
+    }
+
   }
 
 
   private  void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact :contacts){
-      writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(),contact.getGroup()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getGroup()));
+      }
     }
-    writer.close();
+
   }
 
 
